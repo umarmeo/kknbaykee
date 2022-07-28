bdfca91a2117c47c55405c256b8a23b063930e8f
ranawais1 <108676316+ranawais1@users.noreply.github.com>
2022-07-28 14:50:42 +0500

    Add files via upload
    Sale order date field - order date in back dates allowed

--- update_account_module_baykee/__manifest__.py
+++ update_account_module_baykee/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '15.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'account_accountant'],
+    'depends': ['base', 'account', 'account_accountant', 'sale'],
 
     # always loaded
     'data': [
