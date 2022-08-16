3e1c85571e6a230c840592a4d6181cf34fcca1a6
sayyamrazzaq <sayyamrazzaq1998@gmail.com>
2022-08-16 16:03:41 +0500

    Add files via upload
    Payment Process

---
+++ update_payment_process_module_baykee/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Payment Process Module Baykee",
+
+    'summary': """
+        this module can update the payment""",
+
+    'description': """
+        -create a name field
+        -create a amount field which is Monetary
+        -create a purpose field which is text
+        -create a state field which is Selection add all the states on form(draft,manager approval,coo approval,
+        co approval,approved)
+        -Add buttons according to states like manager approval,coo approval,co approval,approve
+        -Add cancel buttons
+        -Add Reset to draft , Reset to Manager , Reset to coo buttons.
+        -Give Access Rights to all buttons.
+    """,
+
+    'author': "Barak Ullah",
+    'website': "http://www.kknetworks.com.pk",
+    'images': ['static/description/icon.png'],
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Amount',
+    'version': '15.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'mail'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/views.xml',
+
+    ],
+
+}
