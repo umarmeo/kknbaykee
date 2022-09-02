8a5afc44504ebe4a647b888e3236a5663dc628cd
ranawais1 <108676316+ranawais1@users.noreply.github.com>
2022-09-02 15:52:48 +0500

    Add files via upload

--- update_purchase_module_baykee/models/models.py
+++ update_purchase_module_baykee/models/models.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class update_purchase_module_baykee(models.Model):
-#     _name = 'update_purchase_module_baykee.update_purchase_module_baykee'
-#     _description = 'update_purchase_module_baykee.update_purchase_module_baykee'
+class update_purchase_module_baykee(models.Model):
+    _inherit = 'purchase.order'
+    _description = 'update_purchase_order'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    account_analytic_id = fields.Many2one('account.analytic.account', string='Analytic Account')
+    analytic_tag_ids = fields.Many2one('account.analytic.tag', string='Analytic Tags')
+
+    def action_temp(self):
+        if len(self.order_line) >= 0:
+            duplicate_list = [self.account_analytic_id, self.analytic_tag_ids]
+            return duplicate_list
+
+class update_purchase_module_baykee(models.Model):
+    _inherit = 'purchase.order.line'
+
+    analytic_tag_ids = fields.Many2one('account.analytic.tag', string='Analytic Tags')
+
+    @api.onchange('account_analytic_id', 'analytic_tag_ids', 'name')
+    def _onchange_receive(self):
+        list_receive = self.order_id.action_temp()
+        self.account_analytic_id = list_receive[0]
+        self.analytic_tag_ids = list_receive[1]
