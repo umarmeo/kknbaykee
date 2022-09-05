86e323e0d6b83a97373c6ed24b61a55b40ee0a15
ranawais1 <108676316+ranawais1@users.noreply.github.com>
2022-09-05 11:43:04 +0500

    Add files via upload

---
+++ sale_reports_baykee/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Reports Baykee",
+
+    'summary': """
+        This Module can be Create the Report of Sales""",
+
+    'description': """
+    """,
+
+    'author': "KKNetworks",
+    'website': "www.kknetworks.com.pk",
+    'category': 'Sales',
+
+    'version': '15.0.1',
+
+    'depends': ['base', 'sale'],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'report/report.xml',
+        'report/sale_person_wise_sale_report_template.xml',
+        'wizard/sale_person_wise_sale_report.xml',
+    ],
+}
---
+++ sale_reports_baykee/wizard/__init__.py
@@ -0,0 +1,1 @@
+from . import sale_person_wise_sale_report
