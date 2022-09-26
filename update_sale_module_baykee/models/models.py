f2641fe76ab67963f01fa53ae6b6e123c805a136
ranawais1 <108676316+ranawais1@users.noreply.github.com>
2022-09-26 14:57:41 +0500

    Add files via upload

--- update_sale_module_baykee/models/models.py
+++ update_sale_module_baykee/models/models.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class update_sale_module_baykee(models.Model):
     _inherit = 'sale.order'
 
-    account_analytic_id = fields.Many2one('account.analytic.account', 'Analytic Account')
     analytic_tag_ids = fields.Many2one('account.analytic.tag', string='Analytic Tags')
-    company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.company, ondelete='cascade', readonly=True)
+    company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.company, ondelete='cascade',
+                                 readonly=True)
     invoice_status = fields.Selection([
         ('upselling', 'Upselling Opportunity'),
         ('invoiced', 'Fully Invoiced'),
@@ -59,19 +60,45 @@ class update_sale_module_baykee(models.Model):
             'state': 'sale'
         }
 
-    @api.onchange('account_analytic_id', 'analytic_tag_ids')
+    @api.onchange('analytic_account_id', 'analytic_tag_ids')
     def _onchange_sale_order_analytic(self):
         for line in self.order_line:
-            line.account_analytic_id = self.account_analytic_id
+            line.analytic_account_id = self.analytic_account_id
             line.analytic_tag_ids = self.analytic_tag_ids
 
     def action_data_send(self):
-        duplicate_list = [self.account_analytic_id, self.analytic_tag_ids]
+        duplicate_list = [self.analytic_account_id, self.analytic_tag_ids]
         return duplicate_list
+
+    def action_confirm(self):
+        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
+            raise UserError(_(
+                'It is not allowed to confirm an order in the following states: %s'
+            ) % (', '.join(self._get_forbidden_state_confirm())))
+
+        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
+            order.message_subscribe([order.partner_id.id])
+        self.write(self._prepare_confirmation_values())
+
+        if self.order_line:
+            for rec in self.order_line:
+                if rec.price_unit >= rec.min_sale_price:
+                    raise UserError(_('Unit price %s of %s must be less than or equal to Minimum Sale Price %s'
+                                      % (rec.price_unit, rec.product_id.name, rec.min_sale_price)))
+
+        # Context key 'default_name' is sometimes propagated up to here.
+        # We don't need it and it creates issues in the creation of linked records.
+        context = self._context.copy()
+        context.pop('default_name', None)
+        self.with_context(context)._action_confirm()
+        if self.env.user.has_group('sale.group_auto_done_setting'):
+            self.action_done()
+        return True
+
 
 class update_sale_order_line(models.Model):
     _inherit = 'sale.order.line'
-    account_analytic_id = fields.Many2one('account.analytic.account', 'Analytic Account')
+    analytic_account_id = fields.Many2one('account.analytic.account', 'Analytic Account')
     analytic_tag_ids = fields.Many2one('account.analytic.tag', string='Analytic Tags')
     state = fields.Selection(
         related='order_id.state', string='Order Status', copy=False, store=True)
@@ -86,10 +113,17 @@ class update_sale_order_line(models.Model):
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
     user_id = fields.Many2one(
         'res.users', string='Salesperson', default=lambda self: self.env.user)
+    min_sale_price = fields.Float(string="Minimum Sale Price", related='product_id.min_sale_price')
 
-    @api.onchange('account_analytic_id', 'analytic_tag_ids', 'order_id')
+    @api.onchange('analytic_account_id', 'analytic_tag_ids', 'order_id')
     def _onchange_sale_order_line(self):
         list_receive = self.order_id.action_data_send()
-        self.account_analytic_id = list_receive[0]
+        self.analytic_account_id = list_receive[0]
         self.analytic_tag_ids = list_receive[1]
 
+    # @api.onchange('price_unit')
+    # def onchange_price_unit(self):
+    #     for rec in self:
+    #         if rec.price_unit > 0:
+    #             if rec.price_unit > rec.min_sale_price:
+    #                 raise UserError(_('Unit price must be less than or equal to Minimum Sale Price'))
