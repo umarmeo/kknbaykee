b9343b12d88c789d655257a01f7d566eedb7e39c
ranawais1 <108676316+ranawais1@users.noreply.github.com>
2022-09-27 14:48:25 +0500

    Add files via upload

--- update_inventory_module_baykee/models/models.py
+++ update_inventory_module_baykee/models/models.py
@@ -3,29 +3,24 @@
 from odoo import models, fields, api
 
 
-# class update_inventory_module_baykee(models.Model):
-#     _name = 'update_inventory_module_baykee.update_inventory_module_baykee'
-#     _description = 'update_inventory_module_baykee.update_inventory_module_baykee'
+class ProductLabelLayout(models.TransientModel):
+    _inherit = 'product.label.layout'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    extra_html = fields.Html('Extra Content', default='"Warranty Void, If Sticker Removed"')
+    print_format = fields.Selection([
+        ('dymo', 'Dymo'),
+        ('2x7xprice', '2 x 7 with price'),
+        ('4x7xprice', '4 x 7 with price'),
+        ('4x12', '4 x 12'),
+        ('4x12xprice', '4 x 12 with price')], string="Format", default='4x12', required=True)
 
+    class ProductTemplate(models.Model):
+        _inherit = 'product.template'
 
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+        min_sale_price = fields.Float(string="Minimum Sale Price")
 
-    min_sale_price = fields.Float(string="Minimum Sale Price")
+    class ProductProduct(models.Model):
+        _inherit = 'product.product'
 
-
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
-
-    min_sale_price = fields.Float(string="Minimum Sale Price")
+        min_sale_price = fields.Float(string="Minimum Sale Price")
 
