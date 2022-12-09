353e22e403c97f725bbf8464db873492dfb124ef
ranawais1 <108676316+ranawais1@users.noreply.github.com>
2022-12-09 10:38:08 +0500

    Delete report directory

--- report/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import journal_entry_report
-from . import cash_and_bank_summary_template
-from . import cash_and_bank_summary_xlsx
-from . import partner_legder_report_template
-from . import partner_ledger_report_xlsx
-from . import general_legder_report_template
-from . import general_ledger_report_xlsx
