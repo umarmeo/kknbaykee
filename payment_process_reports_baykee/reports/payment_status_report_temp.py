625192d9d2800c2a6bde624fc60aa016fb4631b4
ranawais1 <108676316+ranawais1@users.noreply.github.com>
2022-12-30 18:39:54 +0500

    Add files via upload

---
+++ payment_process_reports_baykee/reports/payment_status_report_temp.py
@@ -0,0 +1,51 @@
+from odoo import api, fields, models, _
+from datetime import date, datetime, timedelta
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class PaymentStatusReportTemplate(models.AbstractModel):
+    _name = 'report.payment_process_reports_baykee.payment_status_report'
+    _description = 'Payment Status Report Template'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        data_temp = []
+        docs = self.env['payment.status.wiz'].browse(docids[0])
+        company_id = self.env.user.company_id
+        analytic_account = docs.analytic_account_id.ids if docs.analytic_account_id else []
+        division = docs.division_id.ids if docs.division_id else []
+        payment_mode = docs.payment_mode.id if docs.payment_mode else []
+        temp = []
+        domain = []
+        if docs.state:
+            domain.append(('state', '=', docs.state))
+        if division:
+            domain.append(('division', 'in', division))
+        if analytic_account:
+            domain.append(('account_analytic_id', 'in', analytic_account))
+        if payment_mode:
+            domain.append(('payment_mode', 'in', payment_mode))
+        payment_process = self.env['payment.process'].search(domain).filtered(
+            lambda ln: ln.month == int(docs.month) and ln.year == int(docs.year))
+        for payment in payment_process:
+            vals = {
+                'ref': payment.name,
+                'term': payment.term,
+                'desc': payment.purpose,
+                'status': payment.payment_status,
+                'amount': payment.amount,
+            }
+            temp.append(vals)
+            print(temp)
+        temp2 = temp
+        data_temp.append([temp2])
+        return {
+            'doc_ids': self.ids,
+            'doc_model': 'purchase.order.line',
+            'dat': data_temp,
+            'docs': docs,
+            'company_id': company_id,
+            'data': data,
+        }
---
+++ payment_process_reports_baykee/wizards/payment_status_report_wiz.py
@@ -0,0 +1,46 @@
+from odoo import fields, models, api
+import datetime
+
+
+class PaymentStatusReportWizard(models.TransientModel):
+    _name = 'payment.status.wiz'
+    _description = 'Payment Status Report Wizard'
+
+    month = fields.Selection([
+        ('1', "January"),
+        ('2', "February"),
+        ('3', "March"),
+        ('4', "April"),
+        ('5', "May"),
+        ('6', "June"),
+        ('7', "July"),
+        ('8', "August"),
+        ('9', "September"),
+        ('10', "October"),
+        ('11', "November"),
+        ('12', "December"),
+    ], string="Month")
+
+    @api.model
+    def year_selection(self):
+        year = 2000
+        year_list = []
+        while year != 2090:
+            year_list.append((str(year), str(year)))
+            year += 1
+        return year_list
+
+    year = fields.Selection(year_selection, string="Year", default=datetime.date.today().year)
+    division_id = fields.Many2many('payment.division', string="Division")
+    analytic_account_id = fields.Many2many('account.analytic.account', string="Analytic Account")
+    state = fields.Selection(selection=[
+        ('draft', 'Draft'),
+        ('hod', 'HOD Approval'),
+        ('manager', 'Accounts Approval'),
+        ('coo', 'COO Approval'),
+        ('ceo', 'CEO Approval'),
+        ('approved', 'Approved'),
+        ('cancel', 'Cancelled'),
+        ('reject', 'Reject'),
+    ], string='Status')
+    payment_mode = fields.Many2many('payment.mode', string="Payment Mode")
