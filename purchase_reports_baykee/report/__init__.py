5fb54ccbbbef2005ab88825a4a143d23f5ecbbbe
ranawais1 <108676316+ranawais1@users.noreply.github.com>
2022-12-30 18:31:41 +0500

    Add files via upload

--- purchase_reports_baykee/report/__init__.py
+++ purchase_reports_baykee/report/__init__.py
@@ -1,5 +1,7 @@
 from . import vendor_price_comparison_template
 from . import pending_bills_report_template
 from . import selling_price_variation_template
+from . import party_wise_purchase_template
+
 
 
