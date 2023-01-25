cd1a868bdf46b806e821529ee5264fa3af938aff
ranawais1 <108676316+ranawais1@users.noreply.github.com>
2023-01-25 16:07:11 +0500

    Add files via upload

--- sale_reports_baykee/report/age_receivable_report_template.py
+++ sale_reports_baykee/report/age_receivable_report_template.py
@@ -22,6 +22,47 @@ class AgeReceivableTemplate(models.AbstractModel):
             part_domain += [('id', 'in', partners)]
         partner_search = self.env['res.partner'].search(part_domain)
         for partner in partner_search:
+            temp3 = []
+            account_line_domain = [('date', '<=', date),
+                                   ('partner_id', '=', partner.id),
+                                   ('move_id.state', '=', 'posted'),
+                                   ('move_id.move_type', '=', 'entry'),
+                                   ('account_id', '=', partner.property_account_receivable_id.id)]
+            move_line = self.env['account.move.line'].search(account_line_domain)
+            for move in move_line:
+                as_of = 0
+                a = 0
+                b = 0
+                c = 0
+                d = 0
+                e = 0
+                delta = date - move.date
+                days = delta.days
+                if 0 < days < 1:
+                    as_of = move.amount_currency
+                elif days <= 30:
+                    a = move.amount_currency
+                elif 31 <= days <= 60:
+                    b = move.amount_currency
+                elif 61 <= days <= 90:
+                    c = move.amount_currency
+                elif 91 <= days <= 120:
+                    d = move.amount_currency
+                elif days >= 121:
+                    e = move.amount_currency
+                vals = {
+                    'name': move.move_name,
+                    'due_date': move.date,
+                    'account': partner.property_account_receivable_id.name,
+                    'as_data': as_of,
+                    '1-30': a,
+                    '31-60': b,
+                    '61-90': c,
+                    '91-120': d,
+                    'older': e,
+                    'total': False,
+                }
+                temp3.append(vals)
             temp1 = []
             payment_domain = [('date', '<=', date),
                               ('partner_id', '=', partner.id),
@@ -68,9 +109,9 @@ class AgeReceivableTemplate(models.AbstractModel):
             temp = []
             domain = [('invoice_date', '<=', date),
                       ('partner_id', '=', partner.id),
-                      ('payment_state', '=', 'not_paid'),
+                      ('payment_state', 'in', ['not_paid', 'partial']),
                       ('state', '=', 'posted'),
-                      ('move_type', '=', ['out_invoice', 'out_refund', 'out_receipt', 'entry']),
+                      ('move_type', '=', ['out_invoice', 'out_refund', 'out_receipt']),
                       ('partner_id.property_account_receivable_id', '=', partner.property_account_receivable_id.id)]
             if analytic_accounts:
                 domain.append(('analytic_account_id', '=', analytic_accounts))
@@ -111,7 +152,7 @@ class AgeReceivableTemplate(models.AbstractModel):
                     'total': False,
                 }
                 temp.append(vals)
-            temp2 = temp + temp1
+            temp2 = temp + temp1 + temp3
             data_temp.append(
                 [partner.name, temp2])
         return {
