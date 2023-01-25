6776675a9899d7e94f05c69632fab4dc84d22da6
ranawais1 <108676316+ranawais1@users.noreply.github.com>
2023-01-25 14:15:06 +0500

    Add files via upload

--- sale_reports_baykee/report/__init__.py
+++ sale_reports_baykee/report/__init__.py
@@ -1,2 +1,6 @@
 from . import item_wise_sale_report_template
 from . import sale_person_wise_sale_report_template
+from . import customer_wise_sale_report_template
+from . import age_receivable_report_template
+
+
